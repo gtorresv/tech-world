3969823358f98d330ed47148875a1fb1f4c29085
gtorresv <giselle.torres15@gmail.com>
2023-09-01 22:49:29 -0400

    completed columns in models folder and worked on the api in the category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // be sure to include its associated Products (done!)
+  const categoryData = await Category.findAll({
+    include: [Product],
+  }).catch((err) => {
+    res.json(err);
+  });
+  res.json(categoryData);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // be sure to include its associated Products (done!)
+  const categoryData = await Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product],
+  }).catch((err) => {
+    res.json(err);
+  });
+  res.json(categoryData);
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  // create a new category (done!)
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
